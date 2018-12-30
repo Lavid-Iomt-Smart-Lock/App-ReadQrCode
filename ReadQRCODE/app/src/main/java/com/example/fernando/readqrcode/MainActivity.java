1405278635c09d8f8db86798a71bb242635021ca
Mateus Antonio <mateustoin@gmail.com>
2018-12-30 07:59:23 -0300

    Integração com Firebase implementada

--- ReadQRCODE/app/src/main/java/com/example/fernando/readqrcode/MainActivity.java
+++ ReadQRCODE/app/src/main/java/com/example/fernando/readqrcode/MainActivity.java
@@ -2,24 +2,44 @@ package com.example.fernando.readqrcode;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
     Button btnScan;
+    Button button1;
+    Button button2;
+    TextView textView;
+
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference();
+
+    final DatabaseReference accessStatus = myRef.child("access").child("status");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnScan = (Button) findViewById(R.id.btnScan);
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        textView = (TextView) findViewById(R.id.textView1);
+
         final Activity activity = this;
 
         btnScan.setOnClickListener(new View.OnClickListener() {
@@ -34,6 +54,36 @@ public class MainActivity extends AppCompatActivity {
                 integrator.initiateScan();
             }
         });
+
+        accessStatus.addValueEventListener(new ValueEventListener(){
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot){
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("file", "Value is: " + value);
+                textView.setText(value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error){
+                Log.w("File", "Failed to read value", error.toException());
+            }
+        });
+
+
+        button1.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                accessStatus.setValue("true");
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                accessStatus.setValue("false");
+            }
+        });
+
     }
 
     @Override
@@ -53,4 +103,8 @@ public class MainActivity extends AppCompatActivity {
     private void alert(String msg) {
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
     }
+
+
+
+
 }
